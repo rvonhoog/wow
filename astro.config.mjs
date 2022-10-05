195ab642759174ddffcef70789c99cb5dbd7d4cd
RVonHoog <52821507+rvonhoog@users.noreply.github.com>
2022-10-05 07:33:52 -0700

    Chore: added pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+site: 'https://rvonhoog.github.io',
+ base: '/wow,'
+
+
+});
